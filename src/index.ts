495626bc246197555fca18e92962ab40d06c4497
Mats Roshauw <karmats@gmail.com>
2020-11-14 09:13:56 +0100

    Print in markdown

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import fs from "fs";
 import glob from "glob";
 
 const PROJECT_ARG = "--project";
-const COLOR_REGEX = /(#[0-9a-fA-F]{3,})/gm;
+/* Catches all hex and rgb(a) colors */
+const COLOR_REGEX = /(#[0-9a-fA-F]{3,}|rgba?\(\d+,\s?\d+,\s?\d+,?\s?\d?\))/gm;
+const extractCurrentFilename = (path: string): string => {
+  const splitted = path.split("/");
+  return splitted[splitted.length - 1];
+};
 
 const projectArg = process.argv.find((arg) => arg.startsWith(PROJECT_ARG));
 if (!projectArg) {
@@ -13,17 +18,43 @@ if (!projectArg) {
 }
 
 const projectPath = projectArg.split("=")[1];
-const result: { file: string; color: string }[] = [];
-glob(`${projectPath}/!(node_modules)/**/*.{css,scss}`, (err, fileNames) => {
+const result: { file: string; colors: string[] }[] = [];
+const uniqueColors: string[] = [];
+glob(`${projectPath}/!(node_modules)/**/*.{css,scss}`, (err, filePaths) => {
   if (err) {
     throw new Error(`Failed to read project files: ${err.message}`);
   }
-  for (const fileName of fileNames) {
-    const file = fs.readFileSync(fileName, "utf-8");
-    const res = COLOR_REGEX.exec(file);
-    if (res && res.length) {
-      result.push({ file: fileName, color: res[0] });
+  for (const filePath of filePaths) {
+    const file = fs.readFileSync(filePath, "utf-8");
+    const matches = file.matchAll(COLOR_REGEX);
+    if (COLOR_REGEX.test(file)) {
+      const colors = [];
+      for (const match of matches) {
+        const color = match[0];
+        colors.push(color);
+        if (!uniqueColors.includes(color)) {
+          uniqueColors.push(color);
+        }
+      }
+      result.push({ file: extractCurrentFilename(filePath), colors });
     }
   }
-  console.log(result);
+  result.sort((a, b) => a.file.localeCompare(b.file));
+  console.log(
+    `# ${extractCurrentFilename(projectPath)}
+
+## Unique colors
+
+<div style='background:#777'>
+  ${uniqueColors
+    .sort((a, b) => a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()))
+    .map((c) => `<div style="color: ${c}">${c}</div>`)
+    .join("")}
+</div>
+
+## Colors per file
+
+${result.map((r) => `- ${r.file}\n  - ${r.colors.join("\n  - ")}\n`).join("")}
+`
+  );
 });
